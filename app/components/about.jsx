eafeaae7b9424df5662d69cd75ad99dfad1b63dd
Muhammad Salman <Muhammad Salman>
2016-11-29 17:33:53 +0500

    About Page Change

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -1,5 +1,13 @@
 var React = require('react');
 
-var About = (props) => <h3>About Component</h3>;
+var About = (props) => {
+  return (
+    <div>
+      <h3>About</h3>
+      <p>Wellcome to About Page</p>
+    </div>
+  )
+}
+
 
 module.exports = About;
